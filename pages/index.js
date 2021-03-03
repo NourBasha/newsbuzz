02c57ca5bd499185f9f19ae4c2f5fa6f10d477b4
NourB <nour.basha2011@gmail.com>
2021-03-03 15:44:29 +0200

    structure as requested

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,129 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col,Card } from 'react-bootstrap';
+import Date from '../lib/date';
+import {getNewsData} from '../lib/data';
+import { faRss } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+export default function Home(props) {
+
+  const [page, setPage] = useState(1);
+  const [news, setNews] = useState([]);
+ 
+  useEffect(()=>{
+     if(page> 1){
+      document.addEventListener('scroll', handleScroll);
+      getNewsData(`http://80.240.21.204:1337/news?skip=${page}&limit=10`)
+      .then(res=>{
+        let newArray = [...news, ...res];
+         setNews(newArray);
+      });
+    
+     }
+
+    }
+  ,[page]);
+
+  useEffect(()=>{
+    if(page===1){
+      document.addEventListener('scroll', handleScroll);
+        getNewsData(`http://80.240.21.204:1337/news?skip=${page}&limit=10`).then(res =>{
+        setNews(res);
+      });
+    }
+  },[]);
+
+  const NewsCard = () =>{
+    return(
+        news.map((item)=>
+              <Card key={item._id+Math.random()} className={styles.card} style={{ width: '18rem' }}>
+                {
+                  item
+                  ?<Card.Body className={styles.cardBody}>
+                  <FontAwesomeIcon className={styles.rss} color="teal" icon={faRss} ></FontAwesomeIcon>
+                      <div className={styles.cardHead}>
+                        <img  alt='Image'
+                        src={item.source.url}>
+                        </img>
+                        <Card.Title style={{marginLeft:'20px'}}>{item.source.title}</Card.Title>
+                      </div>
+                      
+                    <Card.Text className={styles.title}>
+                       {item.title}
+                    </Card.Text>
+                    <Card.Text className={styles.date}>
+                     {Date(item.created_at)}
+                    </Card.Text>
+                    {
+                      item.keywords.length > 0 
+                      ? <hr />
+                      :null
+                    }
+    
+                    <div className={styles.keywords}>
+                          {
+                              item.keywords.map(keyword=>
+                                <Card.Text style={{marginLeft:'2px'}} key={keyword._id+Math.random()}>{keyword.name}</Card.Text>
+                              )
+                          }
+                    </div>
+                   
+                  </Card.Body>
+                  :null
+                }
+              
+            </Card>
+        )
+      
+    )
+  }
+
+  function isBottom(el) {
+    return el.getBoundingClientRect().bottom <= window.innerHeight;
+  }
+
+  const handleScroll = () =>{
+     const wrappedElement = document.getElementById('newsCol');
+     if (isBottom(wrappedElement)) {
+       setPage(page+1);
+       document.removeEventListener('scroll', handleScroll);
+     }
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container}  >
+     
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Container  className={styles.container} fluid >
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Row className={styles.row}  >
+        <Col md={2} className={styles.fixed}>           
+        </Col>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Col sm={12} md={8} className={styles.newsCol} id='newsCol' onScroll={handleScroll}>
+             
+              {
+                news.length > 0
+                ?<NewsCard />
+                :null
+              }
+             
+        </Col>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <Col md={2} className={styles.fixed}>  
+        </Col>
+      </Row>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </Container>
+    
+      
     </div>
-  )
+  );
 }
